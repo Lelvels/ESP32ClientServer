fb1cec9bee091984f4dbb6ddb037354675ed9bc8
Lelvels <levels1912@gmail.com>
2022-11-10 11:52:43 +0700

    Them phan dieu khien

--- src/main.cpp
+++ src/main.cpp
@@ -7,10 +7,20 @@
 #include <Wire.h>
 #include <LiquidCrystal.h>
 
+/* Declare any sensor here */
+#define DHTpin 15
+#define DHTType DHT11
+#define MESSAGE_LENGTH 1000
+
+/* Wifi connection */
+const char* ssid = "ThaoAn/2.4G";
+const char* password = "123456789";
+
 const char* JSON_MIME = "application/json";
 const char* TEXT_MIME = "text/plain";
 // Change here to send data
 const int deviceId = 3;
+DHT dht(DHTpin, DHTType);
 
 //String sendDataWebsite = "https://aqua-iot.pro/api/v1/sensordatas";
 String hostname = "http://aqua-iot.pro/api/v1/sensordatas";
@@ -18,14 +28,6 @@ String twinurl = "http://aqua-iot.pro/api/v1/devices/"+String(deviceId);
 unsigned long lastTime = 0;
 unsigned long timerDelay = 10000;
 
-/* Declare any sensor here */
-#define DHTpin 15
-#define DHTType DHT11
-DHT dht(DHTpin, DHTType);
-
-/* Wifi connection */
-const char* ssid = "ThaoAn/2.4G";
-const char* password = "123456789";
 
 void setup(){
     Serial.begin(115200);
@@ -47,24 +49,36 @@ void loop() {
             HTTPClient http;
             //Gửi dữ liệu lên server
             Serial.println("\n[+] Starting post method to send data:");
-            DynamicJsonDocument doc(1000);
+            //Tạo document để thực hiện serialize và deserialize json, xem thêm tại: https://arduinojson.org/
+            DynamicJsonDocument doc(MESSAGE_LENGTH);
             String data_str = "";
 
+            //Cấu trúc tin nhắn gửi dữ liệu
+            /* 
+            {
+                "deviceId": int,
+                "data": string (dạng json)
+            } 
+            */
+            // Bước 1: tạo chuỗi JSON cho trường data
             doc["humidity"] = dht.readHumidity();
             doc["temperature"] = dht.readTemperature();
             serializeJson(doc, data_str);
-    
+
+            //Bước 2: tạo tin nhắn dưới dạng JSON
             doc.clear();
             doc["deviceId"] = deviceId;
             doc["data"] = data_str;
             String sensorData = "";
             serializeJson(doc, sensorData);
 
+            //Bước 3: Setup http client, bao gồm tên miền gửi lên và header
             http.begin(hostname.c_str());
             http.addHeader("Content-Type", JSON_MIME);
             http.addHeader("Connection", "keep-alive");
             http.addHeader("Accept", JSON_MIME);
 
+            //Bước 4: Gửi dữ liệu lên, method là POST rồi in ra
             int respCode = http.POST(sensorData);
             Serial.print("[+] Sending: ");
             Serial.println(sensorData);
@@ -79,21 +93,44 @@ void loop() {
                 Serial.println(respCode);
             }
 
-            //Nhận dữ liệu về
+            //Phần 2: Thực hiện nhận chuỗi điều khiển
             Serial.println("\n[+] Starting get method to receive desired twin:");
+            //Bước 1: Setup http client với hostname
             http.begin(twinurl.c_str());
-            // Send HTTP GET request
+            http.addHeader("Accept", JSON_MIME);
+            //Bước 2: thực hiện phương thức GET để lấy dữ liệu
             int httpResponseCode = http.GET();
             if (httpResponseCode>0) {
+                //Bước 3: Lấy dữ liệu từ chuỗi JSON nhận về, cấu trúc của nó có dạng như sau
+                /* 
+                {
+                    "data": {
+                        "id": 3,
+                        "name": "espcong",
+                        "enabled": "Enabled",
+                        "connectionState": "Disconnected",
+                        "deviceToCloudMessages": 131,
+                        "cloudToDeviceMessages": 0,
+                        "desired": "{\"hello\": \"whatlad\", 
+                            \"desired_ph\": \"7\", 
+                            \"desired_temp\": \"20\", 
+                            \"desired_humid\": \"85\"}",
+                        "reported": "{}"
+                    }
+                }
+                */
                 Serial.print("[+] HTTP Response code: ");
                 Serial.println(httpResponseCode);
                 String payload = http.getString();
-                Serial.println("[+]" + payload);
+                Serial.println("[+] Payload: " + payload);
+                //Phân tích dữ liệu trả về
                 deserializeJson(doc, payload);
-                
+
+                //Lấy dữ liệu desired
                 String desired = doc["data"]["desired"];
-                Serial.println("[+]" + desired);
+                Serial.println("[+] Desired string: " + desired);
                 doc.clear();
+                //Biến dữ liệu desired thành object!
                 deserializeJson(doc, desired);
                 String hello = doc["hello"];
                 int ph = doc["desired_ph"];
