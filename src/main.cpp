377eac03910b186db299eb23f0111b560fbb33aa
Lelvels <levels1912@gmail.com>
2023-02-09 15:40:02 +0700

    Fix general problems

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,17 @@
 #include <Arduino.h>
 #include <ArduinoJson.h>
-#include <DHT.h>
 #include <WiFi.h>
 #include <HTTPClient.h>
 #include <AsyncWebSocket.h>
 #include <Wire.h>
-#include <LiquidCrystal.h>
 
 /* Declare any sensor here */
-#define DHTpin 15
-#define DHTType DHT11
+/* 
+    DHT: pin 15,
+    LED: pin 2 cua ESP32  
+*/
 #define MESSAGE_LENGTH 1000
+#define LEDPin 2
 
 /* Wifi connection */
 const char* ssid = "ThaoAn/2.4G";
@@ -19,12 +20,10 @@ const char* password = "123456789";
 const char* JSON_MIME = "application/json";
 const char* TEXT_MIME = "text/plain";
 // Change here to send data
-const int deviceId = 3;
-DHT dht(DHTpin, DHTType);
+const int deviceId = 7;
 
-//String sendDataWebsite = "https://aqua-iot.pro/api/v1/sensordatas";
-String hostname = "http://aqua-iot.pro/api/v1/sensordatas";
-String twinurl = "http://aqua-iot.pro/api/v1/devices/"+String(deviceId);
+String hostname = "https://aqua-iot.pro/api/v1/sensordatas";
+String twinurl = "https://aqua-iot.pro/api/v1/devices/"+String(deviceId);
 unsigned long lastTime = 0;
 unsigned long timerDelay = 10000;
 
@@ -37,8 +36,7 @@ void setup(){
         Serial.printf("Wifi failed to connect!");
         return;
     }
-    dht.begin();
-
+    pinMode(LEDPin, OUTPUT);
     Serial.printf("IP Address: ");
     Serial.println(WiFi.localIP());
 }
@@ -52,17 +50,16 @@ void loop() {
             //Tạo document để thực hiện serialize và deserialize json, xem thêm tại: https://arduinojson.org/
             DynamicJsonDocument doc(MESSAGE_LENGTH);
             String data_str = "";
-
             //Cấu trúc tin nhắn gửi dữ liệu
             /* 
             {
                 "deviceId": int,
-                "data": string (dạng json)
+                "data": json
             } 
             */
             // Bước 1: tạo chuỗi JSON cho trường data
-            doc["humidity"] = dht.readHumidity();
-            doc["temperature"] = dht.readTemperature();
+            doc["ph"] = random(0, 9);
+            doc["tds"] = random(0, 1000);
             serializeJson(doc, data_str);
 
             //Bước 2: tạo tin nhắn dưới dạng JSON
@@ -105,16 +102,13 @@ void loop() {
                 /* 
                 {
                     "data": {
-                        "id": 3,
-                        "name": "espcong",
-                        "enabled": "Enabled",
-                        "connectionState": "Disconnected",
-                        "deviceToCloudMessages": 131,
-                        "cloudToDeviceMessages": 0,
-                        "desired": "{\"hello\": \"whatlad\", 
-                            \"desired_ph\": \"7\", 
-                            \"desired_temp\": \"20\", 
-                            \"desired_humid\": \"85\"}",
+                        "id": x,
+                        "name": x,
+                        "enabled": x,
+                        "connectionState": x,
+                        "deviceToCloudMessages": x,
+                        "cloudToDeviceMessages": x,
+                        "desired": {"led":true},
                         "reported": "{}"
                     }
                 }
@@ -125,21 +119,10 @@ void loop() {
                 Serial.println("[+] Payload: " + payload);
                 //Phân tích dữ liệu trả về
                 deserializeJson(doc, payload);
-
                 //Lấy dữ liệu desired
                 String desired = doc["data"]["desired"];
                 Serial.println("[+] Desired string: " + desired);
                 doc.clear();
-                //Biến dữ liệu desired thành object!
-                deserializeJson(doc, desired);
-                String hello = doc["hello"];
-                int ph = doc["desired_ph"];
-                int temp = doc["desired_temp"];
-                int humid = doc["desired_humid"];
-                Serial.println("\n[+] PH mong muon: " + String(ph) + 
-                ", Nhiet do mong muon: " + String(temp) + 
-                ", Do am mong muon: " + String(humid) +
-                ", Nguoi dung gui tin nhan: " + String(hello));
             }
             else {
                 Serial.print("[+] Error code: ");
